19f14926a757d832ede95af65229d15596cd3e62
Xingjian <xingjian@pdx.edu>
2020-07-15 21:14:10 -0700

    Make print diamond working

--- Diamond/src/main/java/edu/pdx/cs410J/xingjian/Kata.java
+++ Diamond/src/main/java/edu/pdx/cs410J/xingjian/Kata.java
@@ -2,15 +2,46 @@ package edu.pdx.cs410J.xingjian;
 
 /**
  * A class for getting started with a code kata
- *
+ * <p>
  * Use IntelliJ's "Refactor | Rename..." command to change the name of this
  * class (and its tests).
  */
 public class Kata {
-                                                                                    
 
-  public static void main(String[] args) {
-    System.err.println("Missing command line arguments");
-    System.exit(1);
-  }
+
+    public static void main(String[] args) {
+        System.err.println("Missing command line arguments");
+        System.exit(1);
+    }
+
+    public void printDiamond(char letter) {
+        //char[] letters={"A","B","C","D","E"};
+        int n = letter - 65;
+        for (int i = 0; i <= n; i++) {
+            int temp = i + 65;
+            for (int j = 0; j < 2 * n + 1; j++) {
+                if (j == n - i) {
+                    System.out.print((char) temp);
+                } else if (j == n + i) {
+                    System.out.print((char) temp);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        for (int k = n - 1; k >= 0; k--) {
+            int temp = k + 65;
+            for (int j = 2 * n; j >= 0; j--) {
+                if (j == n - k) {
+                    System.out.print((char) temp);
+                } else if (j == n + k) {
+                    System.out.print((char) temp);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
--- Diamond/src/test/java/edu/pdx/cs410J/xingjian/KataTest.java
+++ Diamond/src/test/java/edu/pdx/cs410J/xingjian/KataTest.java
@@ -10,4 +10,17 @@ public class KataTest
     new Kata();
   }
 
+  @Test
+  public void testC(){
+    Kata obj=new Kata();
+    char temp='C';
+    obj.printDiamond(temp);
+  }
+  @Test
+  public void testE(){
+    Kata obj=new Kata();
+    char temp='E';
+    obj.printDiamond(temp);
+  }
+
 }
